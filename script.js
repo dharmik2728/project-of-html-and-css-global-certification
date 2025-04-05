44600e248ba9105a8a579a6987353f3cd5501f2e
dharmik2728 <dharmikjayswal368@gmail.com>
2025-04-05 15:24:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function maskAadhaar() {
+    const input = document.getElementById('aadhaarInput').value;
+    const output = document.getElementById('maskedOutput');
+  
+    if (input.length !== 12 || isNaN(input)) {
+      output.textContent = "Please enter a valid 12-digit Aadhaar number.";
+      output.style.color = 'red';
+      return;
+    }
+  
+    const last4 = input.slice(-4);
+    const masked = '*******' + last4;
+    output.textContent = `Masked Aadhaar: ${masked}`;
+    output.style.color = 'black';
+  }
+  
